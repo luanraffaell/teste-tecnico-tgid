69de98ead074ba75aac893ffe459e04b1957ee3b
Luan Rafael <luanraffaell@hotmail.com>
2023-09-14 15:21:09 -0300

    feat: Entidade cliente

---
+++ backend/tgid/src/main/java/com/tgid/domain/users/Cliente.java
@@ -0,0 +1,22 @@
+package com.tgid.domain.users;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name= "tb_cliente")
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String CPF;
+
+}
