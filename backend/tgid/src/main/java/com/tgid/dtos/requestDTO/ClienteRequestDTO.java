0afb7a8222050c1912a73743051ce2e51b00230b
Luan Rafael <luanraffaell@hotmail.com>
2023-09-14 20:10:14 -0300

    feat: anotações do bean validation

--- backend/tgid/src/main/java/com/tgid/dtos/requestDTO/ClienteRequestDTO.java
+++ backend/tgid/src/main/java/com/tgid/dtos/requestDTO/ClienteRequestDTO.java
@@ -1,10 +1,17 @@
 package com.tgid.dtos.requestDTO;
 
+import jakarta.persistence.UniqueConstraint;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class ClienteRequestDTO {
+    @NotBlank
     private String nome;
+    @NotBlank
     private String cpf;
+    @NotBlank
+    @Email
     private String email;
 }
--- backend/tgid/src/main/java/com/tgid/dtos/requestDTO/EmpresaRequestDTO.java
+++ backend/tgid/src/main/java/com/tgid/dtos/requestDTO/EmpresaRequestDTO.java
@@ -1,13 +1,20 @@
 package com.tgid.dtos.requestDTO;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Data;
 
 import java.math.BigDecimal;
 @Data
 public class EmpresaRequestDTO {
-
+    @NotBlank
     private String nome;
+    @NotBlank
     private String cnpj;
+    @PositiveOrZero
     private BigDecimal saldo;
+    @NotBlank
+    @Email
     private String email;
 }
