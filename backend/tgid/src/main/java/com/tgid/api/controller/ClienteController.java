baccff9a3c0e6593290170a3acc54c11b3baf9f6
Luan Rafael <luanraffaell@hotmail.com>
2023-09-14 20:31:35 -0300

    feat: bean validation

--- backend/tgid/src/main/java/com/tgid/api/controller/ClienteController.java
+++ backend/tgid/src/main/java/com/tgid/api/controller/ClienteController.java
@@ -1,9 +1,10 @@
-package com.tgid.controller;
+package com.tgid.api.controller;
 
 import com.tgid.domain.users.Cliente;
 import com.tgid.dtos.ClienteDTO;
 import com.tgid.dtos.requestDTO.ClienteRequestDTO;
 import com.tgid.services.ClienteService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +17,7 @@ public class ClienteController {
     private final ClienteService clienteService;
 
     @PostMapping("/cadastrar")
-    public ResponseEntity createCliente(@RequestBody ClienteRequestDTO cliente){
+    public ResponseEntity createCliente(@RequestBody @Valid ClienteRequestDTO cliente){
         return ResponseEntity.status(HttpStatus.CREATED).body(this.clienteService.criarCliente(cliente));
     }
     @GetMapping("/{id}")
--- backend/tgid/src/main/java/com/tgid/api/controller/EmpresaController.java
+++ backend/tgid/src/main/java/com/tgid/api/controller/EmpresaController.java
@@ -1,9 +1,10 @@
-package com.tgid.controller;
+package com.tgid.api.controller;
 
 import com.tgid.domain.users.Empresa;
 import com.tgid.dtos.EmpresaDTO;
 import com.tgid.dtos.requestDTO.EmpresaRequestDTO;
 import com.tgid.services.EmpresaService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +17,7 @@ public class EmpresaController {
     private final EmpresaService empresaService;
 
     @PostMapping("/cadastrar")
-    public ResponseEntity<?> createEmpresa(@RequestBody EmpresaRequestDTO empresa){
+    public ResponseEntity<?> createEmpresa(@RequestBody @Valid EmpresaRequestDTO empresa){
         EmpresaDTO empresaDTO = this.empresaService.criarEmpresa(empresa);
         return ResponseEntity.status(HttpStatus.CREATED).body(empresaDTO);
     }
