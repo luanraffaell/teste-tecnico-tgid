ec71eb90ae56af4659641e22b58ee8e7193064ee
Luan Rafael <luanraffaell@hotmail.com>
2023-09-14 18:49:06 -0300

    feat: criarTransacao service

--- backend/tgid/src/main/java/com/tgid/services/TransacaoService.java
+++ backend/tgid/src/main/java/com/tgid/services/TransacaoService.java
@@ -5,34 +5,70 @@ import com.tgid.domain.transacoes.TransactionType;
 import com.tgid.domain.users.Cliente;
 import com.tgid.domain.users.Empresa;
 import com.tgid.dtos.TransacaoDTO;
+import com.tgid.dtos.TransacaoRequestDTO;
+import com.tgid.repositories.TransacaoRepository;
+import com.tgid.services.factory.TaxaSistema;
 import com.tgid.services.factory.TaxaSistemaFactory;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Service
-@RequiredArgsConstructor
 @AllArgsConstructor
 public class TransacaoService {
-    private EmpresaService empresaService;
-    private ClienteService clienteService;
-    private TaxaSistemaFactory taxaSistemaFactory;
+    private final EmpresaService empresaService;
+    private final ClienteService clienteService;
+    private final TransacaoRepository transacaoRepository;
 
-    public TransacaoDTO criarTransacao(TransacaoDTO transacaoDTO, TransactionType tipoTransacao){
-        Empresa empresaAtual = empresaService.buscarEmpresaPorId(transacaoDTO.getEmpresaId());
-        Cliente clienteAtual = clienteService.buscarClientePorId((transacaoDTO.getClienteId()));
+    private final TaxaSistemaFactory taxaSistemaFactory;
 
-        Transacao transacao = Transacao.builder()
+    public TransacaoDTO criarTransacao(TransacaoRequestDTO transacaoDTO, TransactionType tipoTransacao){
+        Empresa empresaAtual = empresaService.buscarEmpresaPorId(transacaoDTO.empresaId());
+        Cliente clienteAtual = clienteService.buscarClientePorId((transacaoDTO.clienteId()));
+        TaxaSistema taxaSistema = taxaSistemaFactory.getTaxaSistema(tipoTransacao);
+        Transacao.TransacaoBuilder transacao = Transacao.builder()
                 .cliente(clienteAtual)
                 .empresa(empresaAtual)
                 .tipoTransacao(tipoTransacao)
-                .valorTaxa(taxaSistemaFactory.getTaxa(tipoTransacao).aplicarTaxa(transacaoDTO.getValorTaxa()))
-                .timesTamp(LocalDateTime.now())
-                .build();
+                .valorTaxa(taxaSistema.getValorTaxa())
+                .timesTamp(LocalDateTime.now());
+        BigDecimal quantia = taxaSistema.aplicarTaxa(transacaoDTO.valorTransacao());
 
-        return null;
+        saqueDeposito(empresaAtual,quantia,tipoTransacao);
+        transacao.valorTransacao(quantia);
+
+        empresaService.salvarEmpresa(empresaAtual);
+        clienteService.salvarCliente(clienteAtual);
+
+        return salvarTransacao(transacao.build());
+    }
+    @Transactional
+    private TransacaoDTO salvarTransacao(Transacao transacao){
+        return createDTO(this.transacaoRepository.save(transacao));
+    }
+
+    private TransacaoDTO createDTO(Transacao transacao) {
+        return TransacaoDTO.builder()
+                .id(transacao.getId())
+                .timesTamp(transacao.getTimesTamp())
+                .tipoTransacao(transacao.getTipoTransacao())
+                .clienteId(transacao.getCliente().getId())
+                .empresaId(transacao.getEmpresa().getId())
+                .valor(transacao.getValorTransacao())
+                .valorTaxa(transacao.getValorTaxa())
+                .build();
+    }
+
+    private void saqueDeposito(Empresa empresa, BigDecimal quantia, TransactionType tipoTransacao){
+        if(tipoTransacao.equals(TransactionType.SAQUE)){
+            empresa.saque(quantia);
+            return;
+        }
+        empresa.deposito(quantia);
     }
 
 }
