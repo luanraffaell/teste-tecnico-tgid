765f9a2f405f21ade221e9f65bff594baa1610e7
Luan Rafael <luanraffaell@hotmail.com>
2023-09-14 14:59:40 -0300

    feat: projeto criado

---
+++ backend/tgid/src/main/java/com/tgid/DesafioTecnicoTgidApplication.java
@@ -0,0 +1,13 @@
+package com.tgid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTecnicoTgidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTecnicoTgidApplication.class, args);
+	}
+
+}
---
+++ backend/tgid/src/test/java/com/tgid/DesafioTecnicoTgidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTecnicoTgidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
