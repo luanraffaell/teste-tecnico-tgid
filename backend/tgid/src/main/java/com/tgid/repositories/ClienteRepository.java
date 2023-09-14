694d0312cb1788a60a1ff26f405edb74cd06d0fb
Luan Rafael <luanraffaell@hotmail.com>
2023-09-14 15:22:51 -0300

    feat: interface ClienteRepository

---
+++ backend/tgid/src/main/java/com/tgid/repositories/ClienteRepository.java
@@ -0,0 +1,7 @@
+package com.tgid.repositories;
+
+import com.tgid.domain.users.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Cliente,Long> {
+}
