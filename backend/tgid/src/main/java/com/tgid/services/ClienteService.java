071aecf913512ad478ff598c050706e5948b7278
Luan Rafael <luanraffaell@hotmail.com>
2023-09-14 18:46:21 -0300

    fix: cliente service e empresa sevice, retorno dto

--- backend/tgid/src/main/java/com/tgid/services/ClienteService.java
+++ backend/tgid/src/main/java/com/tgid/services/ClienteService.java
@@ -1,19 +1,37 @@
 package com.tgid.services;
 
 import com.tgid.domain.users.Cliente;
+import com.tgid.dtos.ClienteDTO;
+import com.tgid.dtos.requestDTO.ClienteRequestDTO;
 import com.tgid.repositories.ClienteRepository;
 import com.tgid.services.exceptions.EntidadeNaoEncontradaException;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-@RequiredArgsConstructor
 @AllArgsConstructor
 @Service
 public class ClienteService {
-    private ClienteRepository clienteRepository;
+    private final ClienteRepository clienteRepository;
 
     public Cliente buscarClientePorId(Long id){
        return this.clienteRepository.findById(id)
                 .orElseThrow(() -> new EntidadeNaoEncontradaException("Cliente não encontrado!"));
+    }
+    public ClienteDTO buscarClienteDTOPorId(Long id){
+        return new ClienteDTO(this.buscarClientePorId(id));
+    }
+    @Transactional
+    public ClienteDTO salvarCliente(Cliente cliente){
+        return new ClienteDTO(this.clienteRepository.save(cliente));
+    }
+    @Transactional
+    public ClienteDTO criarCliente(ClienteRequestDTO cliente){
+        Cliente novoCliente = Cliente.builder()
+                .CPF(cliente.getCpf())
+                .email(cliente.getEmail())
+                .nome(cliente.getNome())
+                .build();
+        return this.salvarCliente(novoCliente);
     }
 }
--- backend/tgid/src/main/java/com/tgid/services/EmpresaService.java
+++ backend/tgid/src/main/java/com/tgid/services/EmpresaService.java
@@ -1,19 +1,35 @@
 package com.tgid.services;
 
 import com.tgid.domain.users.Empresa;
+import com.tgid.dtos.EmpresaDTO;
+import com.tgid.dtos.requestDTO.EmpresaRequestDTO;
 import com.tgid.repositories.EmpresaRepository;
 import com.tgid.services.exceptions.EntidadeNaoEncontradaException;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-@RequiredArgsConstructor
 @AllArgsConstructor
 @Service
 public class EmpresaService {
-    private EmpresaRepository empresaRepository;
+    private final EmpresaRepository empresaRepository;
 
     public Empresa buscarEmpresaPorId(Long id){
         return this.empresaRepository.findById(id)
                 .orElseThrow(() -> new EntidadeNaoEncontradaException("Empresa não encontrada!"));
     }
+    @Transactional
+    public Empresa salvarEmpresa(Empresa empresa){
+        return this.empresaRepository.save(empresa);
+    }
+    @Transactional
+    public EmpresaDTO criarEmpresa(EmpresaRequestDTO empresaRequest){
+        Empresa empresa = Empresa.builder()
+                .email(empresaRequest.getEmail())
+                .CNPJ(empresaRequest.getCnpj())
+                .nome(empresaRequest.getNome())
+                .saldo(empresaRequest.getSaldo())
+                .build();
+        return new EmpresaDTO(this.salvarEmpresa(empresa));
+    }
+
 }
