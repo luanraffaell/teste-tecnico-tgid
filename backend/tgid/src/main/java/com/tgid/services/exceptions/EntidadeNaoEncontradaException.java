8743e4c13e44e29d4d96b92528ea5153d92c4661
Luan Rafael <luanraffaell@hotmail.com>
2023-09-14 16:36:09 -0300

    feat: exceção de entidade não encontrada

---
+++ backend/tgid/src/main/java/com/tgid/services/exceptions/EntidadeNaoEncontradaException.java
@@ -0,0 +1,7 @@
+package com.tgid.services.exceptions;
+
+public class EntidadeNaoEncontradaException extends RuntimeException{
+    public EntidadeNaoEncontradaException(String message) {
+        super(message);
+    }
+}
